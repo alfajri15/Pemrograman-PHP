1682483575e3aaa72d8e606821f0fca6d2c32f84
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-12-19 14:52:49 +0700

    Update dbkoneksi.php

--- P13/crud_2/models/dbkoneksi.php
+++ P13/crud_2/models/dbkoneksi.php
@@ -1,18 +1,16 @@
 <?php 
-    $dbhost = "localhost";
-    $dbname = "dbbarang";
-    $dbuser = "root";
-    $dbpass = "";
-
-    $dsn = "mysql:host=$dbhost;dbname=$dbname";
+    $dbhost="localhost";
+    $dbname="dbbarang";
+    $dbuser="root";
+    $dbpass="";
+    
+    $dsn="mysql:host=$dbhost;dbname=$dbname";
 
     try {
         $dbh = new PDO($dsn, $dbuser, $dbpass);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-        // echo 'Alhamdulillah Sukses Koneksi Database';
     } catch (PDOException $e){
         echo 'Connection Failed : '.$e->getMessage();
     }
-
 ?>
