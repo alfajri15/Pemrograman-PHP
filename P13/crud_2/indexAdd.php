e7c68df3e1fd020ffa2143bec84b14ec827ac3db
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-12-19 14:55:21 +0700

    Create indexAdd.php

---
+++ P13/crud_2/indexAdd.php
@@ -0,0 +1,11 @@
+<?php 
+    include_once 'header.php';
+    include_once 'sidebar.php';
+?>
+<?php
+    include_once 'models/koneksi.php';
+    include_once 'formBarang.php';
+?>
+<?php 
+    include_once 'footer.php';
+?>
