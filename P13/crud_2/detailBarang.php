0acbee893835edc1a251fb794066f772741805d6
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-12-19 15:45:05 +0700

    Create detailBarang.php

---
+++ P13/crud_2/detailBarang.php
@@ -0,0 +1,21 @@
+<?php 
+    include_once 'header.php';
+    include_once 'sidebar.php';
+?>
+<?php 
+    
+    include_once 'models/koneksi.php';
+    include_once 'd_barang.php';
+    //tangkap request menu di url (index.php?hal=.....)
+    $hal = $_REQUEST['hal'];
+    //jika ada request dari menu di url tampilkan hal sesuai request
+    if(!empty($hal)){
+        include_once $hal.'.php';
+    }
+    else{//jika tidak ada request dari menu di url tampilkan hal home.php
+        include_once 'home.php';
+    }
+?>
+<?php 
+    include_once 'footer.php';
+?>
