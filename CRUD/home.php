b19f02ebc66a710d099a959e9274c97e09347e69
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-12-20 10:46:46 +0700

    Update home.php

--- CRUD/home.php
+++ CRUD/home.php
@@ -1,3 +1,5 @@
 // Tombol tambah
 <br>
 <a class="btn mt-2" style="background-color:#17a2b8; color:#ffffff" href="f_Produk.php"><i class="fa fa-plus nav-icon mr-2"></i>Tambah Produk</a>
+
+method="POST" action="controllers/BarangController.php"
